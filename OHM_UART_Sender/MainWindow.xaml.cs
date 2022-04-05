dc734ab19264e017986ff787e144195106e653bd
Chandler Klüser <Chandler Klser@DESKTOP-ANOFC68>
2022-04-04 21:50:18 -0300

    Adicionar arquivos de projeto.

---
+++ OHM_UART_Sender/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Management;
+using System.Management.Instrumentation;
+using System.IO.Ports;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+
+namespace OHM_UART_Sender
+{
+    public partial class MainWindow : Window
+    {
+        public bool running;
+        public Thread t;
+        public MainWindow()
+        {
+            running = false;
+            InitializeComponent();
+            Button_Start_Stop.Content = "Start";
+        }
+
+        private void Button_Start_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.running)
+            {
+                Button_Start_Stop.Content = "Start";
+                this.running = false;
+                t.Abort();
+                t = null;
+            }
+            else
+            {
+                Button_Start_Stop.Content = "Stop";
+                this.running = true;
+                if (t==null)
+                {
+                    string str1 = TextBox_COM_Port.Text;
+                    Int32 int_1 = int.Parse(TextBox_BAUD_RATE.Text);
+                    Int32 int_2 = int.Parse(TextBox_REFRESH_TIME.Text);
+                    t = new Thread(() => Program.Main(str1, int_1, int_2));
+                    t.Start();
+                }
+            }
+        }
+    }
+}
---
+++ openhardwaremonitor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Hardware;
+using System.IO.Ports;
+
+
+namespace OpenHardwareMonitor {
+  public static class Program {
+
+    [STAThread]
+    public static String GetSystemInfo() {
+      Computer computer = new Computer();
+      computer.GPUEnabled = true;
+      computer.Open();
+      String aux = computer.Hardware[0].Sensors[0].Value.ToString();
+      computer.Close();
+      return aux;
+    }
+    public static void Main(String COM_PORT, Int32 BAUD_RATE, Int32 REFRESH_TIME) {
+      SerialPort port = new SerialPort(COM_PORT, BAUD_RATE, Parity.None, 8, StopBits.One);
+      String str = null;
+      port.Encoding = System.Text.Encoding.UTF8;
+      while (true) {
+        port.Open();
+        str = GetSystemInfo();
+        port.Write(str);
+        port.Close();
+        Thread.Sleep(REFRESH_TIME);
+      }
+    }
+  }
+}
